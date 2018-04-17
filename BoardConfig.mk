8efcbc10b8213a632304f44761c200ba0af1879e
Alin Jerpelea <alin.jerpelea@sony.com>
2018-04-17 16:43:19 +0200

    remove deprecated PRODUCT_PLATFORM
    since we have TARGET_BOOTLOADER_BOARD_NAME there is no need to
    have another propery with the same value
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,9 +16,6 @@ include device/sony/nile/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := nile
 
-# Platform
-PRODUCT_PLATFORM := nile
-
 # NFC
 NXP_CHIP_TYPE := PN553
 NXP_CHIP_FW_TYPE := PN553
